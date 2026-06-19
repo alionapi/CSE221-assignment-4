195df461cf70f84b5966dff14ca1235cf4cbea7e
Aliona Pirozhenko <158124667+alionapi@users.noreply.github.com>
2026-06-20 07:16:19 +0900

    Add files via upload

---
+++ student_database.h
@@ -0,0 +1,23 @@
+#ifndef STUDENT_DATABASE_H
+#define STUDENT_DATABASE_H
+
+#include "student_map.h"
+#include "student_ordered_map.h"
+#include <stdexcept>
+
+class StudentDatabase {
+public:
+    void add_student(int student_id, int score);
+    void update_score(int student_id, int new_score);
+    int get_score(int student_id) const;
+    int get_student(int score) const;
+    void remove_student(int student_id);
+    int** get_top_k_students(int k);
+    int get_rank(int score) const;
+
+private:
+    StudentMap map;
+    StudentOrderedMap ordered_map;
+};
+
+#endif
